e7a77fd8c15263986029e718efa278219a07a721
icehongsi <52072412+icehongsi@users.noreply.github.com>
2020-09-16 16:49:49 +0900

    Update recursion_new.py

--- Algorithms/recursion_new.py
+++ Algorithms/recursion_new.py
@@ -1,14 +1,11 @@
 def min_max2(A):
     if len(A) == 1:
         return A[0], A[0]
-    elif len(A) == 2:
-        return (A[0], A[1]) if A[0] < A[1] else (A[1], A[0])
-    else: #when length of A is bigger than 2
-        pivot = len(A)//2
-        lower, upper = min_max2(A[:pivot]), min_max2(A[pivot:]) # split into half - lower / upper
-        temp_low = lower[0] if lower[0] < upper[0] else upper[0] # retrieve lowest value by comparison
-        temp_high = upper[1] if lower[1] < upper[1] else lower[1] # retrieve HIGHEST value by comparison
-        return temp_low, temp_high
+    pivot = len(A)//2
+    lower, upper = min_max2(A[:pivot]), min_max2(A[pivot:]) # split into half - lower / upper
+    temp_low = lower[0] if lower[0] < upper[0] else upper[0] # retrieve lowest value by comparison
+    temp_high = upper[1] if lower[1] < upper[1] else lower[1] # retrieve HIGHEST value by comparison
+    return temp_low, temp_high
 
 a = list(map(int, input().split()))
 
